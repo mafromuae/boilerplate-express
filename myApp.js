adaedaf1306ebd3a41cc067fddf65369c8c9350c
Mohamed Saeed <mafromuae@gmail.com>
2025-08-09 16:15:19 +0400

    Add console.log to myApp.js

--- myApp.js
+++ myApp.js
@@ -3,39 +3,6 @@ console.log("Hello World");
 let express = require('express');
 let app = express();
 
+// Don't call app.listen() here in FCC challenges
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
