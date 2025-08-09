2cbdc4cc9ac621875800fb8be6bfc2d07e0bbd69
Mohamed Saeed <mafromuae@gmail.com>
2025-08-09 17:46:54 +0400

    update 5

--- myApp.js
+++ myApp.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
+app.get('/json', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.json({"message": "Hello json"})
 });
 
 
