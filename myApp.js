feed8c0629e800428dc218df7c065ced73d88621
Mohamed Saeed <mafromuae@gmail.com>
2025-08-09 17:07:19 +0400

    new changes

--- myApp.js
+++ myApp.js
@@ -3,7 +3,8 @@ console.log("Hello World");
 let express = require('express');
 let app = express();
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
     res.send('Hello Express');
   });
+  
 module.exports = app;
