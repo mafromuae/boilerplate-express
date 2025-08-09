f412e00c3dab0d8b623d1eba26a750ba95a8be6f
Mohamed Saeed <mafromuae@gmail.com>
2025-08-09 19:57:39 +0400

    update 10

--- myApp.js
+++ myApp.js
@@ -5,22 +5,20 @@ const path = require('path');
 
 const app = express();
 
-app.get('/now', function(req, res, next) {
-    req.time = new Date().toString();
-    next();
-  }, function(req, res) {
-    res.json({ time: req.time });
-  });
-  
-  
-
+// Logger middleware should come first to log all requests
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
-    next();
-  });
-  
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({ time: req.time });
+});
 
 // Serve index.html on root path
 app.get('/', (req, res) => {
