a4b84a9f92fe06a6b1c9d373438ca3c3b753ebc6
Mohamed Saeed <mafromuae@gmail.com>
2025-08-09 17:58:19 +0400

    update 6

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 
@@ -5,11 +7,18 @@ const app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/json', (req, res) => {
+// Serve index.html on root path
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
-  res.json({"message": "Hello json"})
 });
 
-
+// Serve JSON on /json path
+app.get('/json', (req, res) => {
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase();
+  }
+  res.json({ message: message });
+});
 
 module.exports = app;
